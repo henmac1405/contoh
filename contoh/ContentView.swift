e6869fab4f6efc35118727f67424ef19b6efdede
Henmac <henmac1405@gmail.com>
2026-04-30 12:57:51 +0700

    Initial Commit

---
+++ contoh/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  contoh
+//
+//  Created by user on 30/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ contoh/contohApp.swift
@@ -0,0 +1,17 @@
+//
+//  contohApp.swift
+//  contoh
+//
+//  Created by user on 30/04/26.
+//
+
+import SwiftUI
+
+@main
+struct contohApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
