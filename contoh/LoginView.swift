74267ff2a3886cae02992e572be2b5957da7bc30
Henmac <henmac1405@gmail.com>
2026-04-30 16:53:04 +0700

    update LoginView

--- contoh/LoginView.swift
+++ contoh/LoginView.swift
@@ -1,8 +1,74 @@
-//
-//  LoginView.swift
-//  contoh
-//
-//  Created by user on 30/04/26.
-//
+ 
+import SwiftUI
 
-import Foundation
+struct LoginView: View {
+    @EnvironmentObject var services : Services
+    @State private var username : String = ""
+    @State private var password : String = ""
+    @State var isEmptyField = false
+    
+    var body: some View {
+        
+        
+        VStack(alignment: .leading){
+            Image("gembok3")
+                .resizable()
+                .frame(width: 200, height: 200)
+                .padding()
+            Text("User Name")
+            TextField("username", text : $username)
+                .padding()
+                .cornerRadius(5)
+                .background(.white)
+                .autocapitalization(.none)
+                .keyboardType(.emailAddress)
+            
+            Text("Password")
+            SecureField("password", text : $password)
+                .padding()
+                .cornerRadius(5)
+                .background(.white)
+                .autocapitalization(.none)
+            
+            // salah password
+            if(!self.services.isCorrect){
+                Text("Username dan password salah").foregroundColor(.red)
+            } else {
+                Text("").foregroundColor(.white)
+            }
+            
+            // field kosong
+            if (self.isEmptyField == true) {
+                Text("Username dan password tidak boleh kosong").foregroundColor(.red)
+            }
+             
+            
+            // Sign in Button
+            HStack{
+                Spacer()
+                Button(action:{
+                    if (self.username.isEmpty || self.password.isEmpty ) {
+//                        isEmptyField = true
+                        self.services.showAlert = true
+                        self.services.messageAlert = "Username dan password tidak boleh kosong OKE"
+                    } else {
+                        self.services.isLoggedIn = true
+                        self.services.isLoading = true
+                         
+//                        self.userAuth.cekLogin(user_id: self.username, user_pin: self.password)
+                    }
+                }){
+                    Text("Login").bold().font(.callout).foregroundColor(.white)
+                }
+                Spacer()
+            }
+            .padding()
+            .background(Color.blue)
+            .cornerRadius(15)
+            
+             
+            
+            
+        }.padding(30)
+    }
+}
