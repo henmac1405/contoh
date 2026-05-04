1688c329c445421825fae148be81f744d73d329b
Henmac <henmac1405@gmail.com>
2026-05-04 10:40:43 +0700

    tes github loginview

--- contoh/LoginView.swift
+++ contoh/LoginView.swift
@@ -7,6 +7,7 @@ struct LoginView: View {
     @State private var password : String = "rahasia"
     @State var isEmptyField = false
     let lightGrayColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0 )
+    //tes GITHUB
     
     var body: some View {
         
