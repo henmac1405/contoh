b27c8b1b7518a123d5323f3b6c890029bbf878aa
Henmac <henmac1405@gmail.com>
2026-05-06 11:08:22 +0700

    update terbaru

--- contoh/BranchView.swift
+++ contoh/BranchView.swift
@@ -6,23 +6,29 @@ struct BranchView: View {
     @EnvironmentObject var controller : Controller
     
     var body: some View {
-        // Cukup gunakan container biasa seperti VStack, List, atau ScrollView
-        ScrollView{
-            VStack(alignment: .leading, spacing: 10) {
-                
-                ForEach(self.controller.dataBranch, id: \.branch_id) { item in
-                    HStack{
-                        Text("\(item.branch_id)").padding(.leading, 10)
-                        Text("\(item.branch_name)").padding(.leading, 10)
+        NavigationView{
+            ScrollView{
+                VStack(alignment: .leading, spacing: 10) {
+                    
+                    ForEach(self.controller.dataBranch, id: \.branch_id) { item in
+                        
+                        NavigationLink(destination: BranchDetilView(branch: item)){
+                            
+                            HStack{
+                                Text("\(item.branch_id)").padding(.leading, 10)
+                                Text("\(item.branch_name)").padding(.leading, 10)
+                            }
+                        }
+                         
                     }
-                     
+                    
                 }
-                
+                .padding()
+                .navigationTitle("Data Branch") // Judul muncul di posisi yang benar
+                .navigationBarTitleDisplayMode(.inline) // Gaya judul kecil di tengah (lebih rapi)
             }
-            .padding()
-            .navigationTitle("Data Branch") // Judul muncul di posisi yang benar
-            .navigationBarTitleDisplayMode(.inline) // Gaya judul kecil di tengah (lebih rapi)
         }
+        
     }
 }
 
--- contoh/ContentView.swift
+++ contoh/ContentView.swift
@@ -1,9 +1,4 @@
-//
-//  ContentView.swift
-//  contoh
-//
-//  Created by user on 30/04/26.
-//
+
 
 import SwiftUI
 
