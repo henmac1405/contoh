43e0b9400e2d81c219b788e7f4a72b17eb63eb7b
henmac1405 <henmac1405@gmail.com>
2026-05-15 16:50:10 +0700

    nambah data user

---
+++ contoh/UserView.swift
@@ -0,0 +1,34 @@
+
+import Foundation
+import SwiftUI
+
+struct DeviceView: View {
+    @EnvironmentObject var controller : Controller
+    
+    var body: some View {
+        NavigationView{
+            ScrollView{
+                VStack(alignment: .leading, spacing: 10) {
+                    
+                    ForEach(self.controller.dataBranch, id: \.branch_id) { item in
+                        
+                        NavigationLink(destination: BranchDetilView(branch: item)){
+                            
+                            HStack{
+                                Text("\(item.branch_id)").padding(.leading, 10)
+                                Text("\(item.branch_name)").padding(.leading, 10)
+                            }
+                        }
+                         
+                    }
+                    
+                }
+                .padding()
+                .navigationTitle("Data Branch") // Judul muncul di posisi yang benar
+                .navigationBarTitleDisplayMode(.inline) // Gaya judul kecil di tengah (lebih rapi)
+            }
+        }
+        
+    }
+}
+
